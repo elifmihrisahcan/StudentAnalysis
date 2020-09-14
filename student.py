29e611635345330fe68b0a0cfd5b33c7beb6c259
Enes Korkmaz <aeneskrkmz@gmail.com>
2020-09-14 13:32:32 +0300

    düzenlenecek

---
+++ student.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+adp = pd.read_excel("DATA\MAPdata.xlsx")
+
+sns.heatmap(adp.corr(), annot = True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
